e757b62814af409f8c0e12a610c6c9895dc9df88
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-10-02 15:50:42 +0100

    Update celery.py

--- bloggin_system/bloggin_system/celery.py
+++ bloggin_system/bloggin_system/celery.py
@@ -1,6 +1,6 @@
-from __future__ import absolute_import, unicode_literals
-import os
 from celery import Celery
+import os
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloggin_system.settings')
 
